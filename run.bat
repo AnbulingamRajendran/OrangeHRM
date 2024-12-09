acd837d00ead9dc968553fa3bf76cd9cd808f76a
Anbulingam <anbulingam.rajendran@gmail.com>
2024-12-09 11:31:15 +0530

    some changes in run.bat

--- run.bat
+++ run.bat
@@ -1,10 +1,10 @@
 @echo off
 rem Run sanity test cases
 echo Running sanity test cases...
-pytest -m "sanity" --html=Reports/report.html TestCases/
+pytest -v -s -m "sanity" --html=Reports/report.html TestCases/
 
 rem Run regression test cases
 echo Running regression test cases...
-pytest -m "regression" --html=Reports/report_regression.html TestCases/
+pytest -v -s -m "regression" --html=Reports/report_regression.html TestCases/
 
 pause
