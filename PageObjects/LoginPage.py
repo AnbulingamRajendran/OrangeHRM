89166d25a581ac4c79d6acf2cef402e2ca6801b4
Anbulingam <anbulingam.rajendran@gmail.com>
2025-02-03 15:07:03 +0530

    03/02/2025

--- PageObjects/LoginPage.py
+++ PageObjects/LoginPage.py
@@ -2,6 +2,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
+# loginpage
+
 class Login:
     user_name_xpath = "//input[@name='username']"
     password_xpath = "//input[@placeholder='Password']"
